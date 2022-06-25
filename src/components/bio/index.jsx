cd2f3d96b90c3f1532dc73f1a015cd741e223313
HyeokE <jhjeong00@gmail.com>
2022-06-25 23:09:23 +0900

    chore: add resume

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -24,9 +24,9 @@ export const Bio = forwardRef((props, ref) => {
               />
               <div className="author-name">
                 <span className="author-name-prefix">Written by</span>
-                <Link to={'/about'} className="author-name-content">
+                <a href={'https://hyeoke.github.io/resume/'} className="author-name-content">
                   <span>@{author}</span>
-                </Link>
+                </a>
                 <div className="author-introduction">{introduction}</div>
                 <p className="author-socials">
                   {social.instagram && (
