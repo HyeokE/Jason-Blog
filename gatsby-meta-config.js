c24f9bc2db250bcafa40cd104a00f44516ecbd75
HyeokE <jhjeong00@gmail.com>
2022-06-25 21:27:50 +0900

    chore: add post

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `Jason's Log`,
+  title: `Jason's DevLog`,
   description: `공부하면서 얻은 지식들을 정리해보려고 해요.`,
   author: `Jason Jeong`,
   introduction: `가독성이 좋은 글을 쓰고 싶어요. 배운 내용들을 문서화 하려고 노력하고 있어요.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://jason-log.vercel.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `HyeokE`, // Your GitHub account
