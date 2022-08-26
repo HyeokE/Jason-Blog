6261e12cbe251aceb236d1ab8a5ab3ca0174a2b6
HyeokE <jhjeong00@gmail.com>
2022-08-27 02:06:33 +0900

    feat: add adsense

--- src/html.js
+++ src/html.js
@@ -13,6 +13,10 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
           {this.props.headComponents}
+          <script async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3261859533984772"
+                  crossOrigin="anonymous">
+          </script>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
